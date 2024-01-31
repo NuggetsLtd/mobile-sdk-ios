efbdfe9b4060c0ec7f576f8e20f95f3bcf487198
Andrew Lord <andrew.lord@nuggets.life>
2024-01-31 12:15:06 +0000

    Update iOS SDK

---
+++ Frameworks/NuggetsSDK.xcframework/ios-arm64/NuggetsSDK.framework/Headers/NuggetsSDK.h
@@ -0,0 +1,17 @@
+//
+//  NuggetsSDK.h
+//  NuggetsSDK
+//
+//  Created by Andrew Lord on 29/01/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NuggetsSDK.
+FOUNDATION_EXPORT double NuggetsSDKVersionNumber;
+
+//! Project version string for NuggetsSDK.
+FOUNDATION_EXPORT const unsigned char NuggetsSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NuggetsSDK/PublicHeader.h>
+
