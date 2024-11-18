62bc76fe0496f3885ba3afb1f890cddf6f4dbe35
Samuel Johnson <155970973+samnuggets@users.noreply.github.com>
2024-11-18 09:32:22 +0000

    Update Android SDK

--- Frameworks/NuggetsSDK.xcframework/ios-x86_64-simulator/NuggetsSDK.framework/Headers/NuggetsSDK-Swift.h
+++ Frameworks/NuggetsSDK.xcframework/ios-x86_64-simulator/NuggetsSDK.framework/Headers/NuggetsSDK-Swift.h
@@ -305,7 +305,7 @@ typedef unsigned int swift_uint4  __attribute__((__ext_vector_type__(4)));
 SWIFT_CLASS("_TtC10NuggetsSDK24NuggetsSDKViewController")
 @interface NuggetsSDKViewController : UIViewController
 - (void)loadView;
-- (void)viewDidDisappear:(BOOL)animated;
+- (void)viewWillDisappear:(BOOL)animated;
 - (nonnull instancetype)initWithNibName:(NSString * _Nullable)nibNameOrNil bundle:(NSBundle * _Nullable)nibBundleOrNil OBJC_DESIGNATED_INITIALIZER;
 - (nullable instancetype)initWithCoder:(NSCoder * _Nonnull)coder OBJC_DESIGNATED_INITIALIZER;
 @end
